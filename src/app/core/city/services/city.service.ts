f2dfa16d76447bcb5c0f16a67a4553052b48f77f
michelguilhen <guilhenmichel@gmail.com>
2024-03-24 21:00:23 -0600

    use environments in services

--- src/app/core/city/services/city.service.ts
+++ src/app/core/city/services/city.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CityOption } from '../interfaces/CityOption';
-import { environment } from '../../../../environments/environment.dev';
+import { environment } from '../../../../environments/environment';
 import { City } from '../interfaces/City';
 
 @Injectable({
