78d6df162478f0889906ed97799568ba82084cb3
michelguilhen <guilhenmichel@gmail.com>
2024-03-24 20:58:41 -0600

    env config

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiUrl: 'http:todo:3000/api',
-  };
-  
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://ec2-34-227-143-251.compute-1.amazonaws.com/api',
+};
